5179d8e3a1db653e0eaa8d864ada38c779e7f260
jfincken-ngc <39104078+jfincken-ngc@users.noreply.github.com>
2018-05-15 13:05:09 -0400

    made app.py infinite
    prints words and sleeps every 1s

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print ("This is the old tie python file-JJ")
+from time import sleep
+print ("This is the start of old tie python file-JJ")
+
+while True:
+   print ("while true endless loop")
+   sleep(1)
