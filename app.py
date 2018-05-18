865e9e79da7744126590f613c928f0a5bf1ff1bd
jfincken-ngc <39104078+jfincken-ngc@users.noreply.github.com>
2018-05-18 14:03:19 -0400

    10 minute loops
    Testing to see what happens when pods finish after 10 minutes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from time import sleep
 print ("This is the start of old tie python file-JJ")
 
 loopCount = 0
-while loopCount < 3600:
+while loopCount < 600:
    print ("looping "+ loopCount + "times")
    sleep(1)
    loopCount += 1
