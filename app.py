502fc5494b249082d57a252ecd0aaa574e86b461
jfincken-ngc <39104078+jfincken-ngc@users.noreply.github.com>
2018-05-18 14:38:24 -0400

    1 minute timeout now
    GO GO GO GO

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from time import sleep
 print ("This is the start of old tie python file-JJ")
 
 loopCount = 0
-while loopCount < 600:
+while loopCount < 60:
    print ("new loop ", loopCount)
    sleep(1)
    loopCount += 1
