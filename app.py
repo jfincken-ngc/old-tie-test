21c39fe222d02082e2f3b4d8b9f689a8df891393
jfincken-ngc <39104078+jfincken-ngc@users.noreply.github.com>
2018-05-23 11:34:06 -0400

    Loop in app.py lasts 15 minutes (900s) now
    Seeing how changing the container loop length affects Pod performance.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from time import sleep
 print ("This is the start of old tie python file-JJ")
 
 loopCount = 0
-while loopCount < 300:
+while loopCount < 900:
    print ("new loop ", loopCount)
    sleep(1)
    loopCount += 1
